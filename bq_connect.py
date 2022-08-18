d8b17d6394f0da47fadf1789680240ea0fc34aa4
unknown <monicahsu@hannstar.com>
2022-08-18 11:32:35 +0800

    Uodated bq_connect.py

---
+++ bq_connect.py
@@ -0,0 +1,24 @@
+from google.cloud import bigquery as bq
+import os
+import glob
+
+# Specify environments
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = glob.glob("*.json")[0]
+project_id = 'bq-test-359202'   # uplowcase sensitive
+client = bq.Client(project=project_id)
+print(client)
+
+# SQL language
+sql = client.query('''
+  SELECT 
+    year
+  FROM 
+    `bigquery-public-data.noaa_gsod.gsod1929`
+  LIMIT
+    10
+    ''')
+
+# Output result
+for row in sql.result():
+    print(row)
+
